278a0a1a8a353c1cec100bc1fb4ff463409c8f24
euni-bit <seunielis@gmail.com>
2024-09-12 16:55:04 -0400

    fixing output

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -17,12 +17,12 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
+        if i % 3 == 0 and i % 5 == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 3 == 0):
-            tmp_results.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
+        elif i % 3 == 0:
+            tmp_result.append("Fizz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
@@ -37,3 +37,4 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
+    
